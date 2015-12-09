83335cfc691569f5ebe046110b624de132848f87
egaus <evan.gaustad@gmail.com>
2015-12-09 06:53:46 -0600

    Dynamic module loading working, file monitoring needs a bit of work

---
+++ lens-linux.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import lens
+import os
+import atexit
+from signal import SIGTERM
+
+
+class Daemon:
+    """
+    A generic daemon class.
+
+    Usage: subclass the Daemon class and override the run() method
+    """
+    def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null',
+                 stderr='/dev/null'):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self.pidfile = pidfile
+
+    def daemonize(self):
+        """
+        do the UNIX double-fork magic, see Stevens' "Advanced
+        Programming in the UNIX Environment" for details (ISBN 0201563177)
+        http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
+        """
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit first parent
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("fork #1 failed: %d (%s)\n" %
+                             (e.errno, e.strerror))
+            sys.exit(1)
+
+        # decouple from parent environment
+        os.chdir("/")
+        os.setsid()
+        os.umask(0)
+
+        # do second fork
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit from second parent
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("fork #2 failed: %d (%s)\n" %
+                             (e.errno, e.strerror))
+            sys.exit(1)
+
+        # redirect standard file descriptors
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = file(self.stdin, 'r')
+        so = file(self.stdout, 'a+')
+        se = file(self.stderr, 'a+', 0)
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+
+        # write pidfile
+        atexit.register(self.delpid)
+        pid = str(os.getpid())
+        file(self.pidfile, 'w+').write("%s\n" % pid)
+
+    def delpid(self):
+        os.remove(self.pidfile)
+
+    def start(self):
+        """
+        Start the daemon
+        """
+        # Check for a pidfile to see if the daemon already runs
+        try:
+            pf = file(self.pidfile, 'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if pid:
+            message = "pidfile %s already exist. Daemon already running?\n"
+            sys.stderr.write(message % self.pidfile)
+            sys.exit(1)
+
+        # Start the daemon
+        self.daemonize()
+        self.run()
+
+    def stop(self):
+        """
+        Stop the daemon
+        """
+        # Get the pid from the pidfile
+        try:
+            pf = file(self.pidfile, 'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if not pid:
+            message = "pidfile %s does not exist. Daemon not running?\n"
+            sys.stderr.write(message % self.pidfile)
+            # not an error in a restart
+            return
+
+        # Try killing the daemon process
+        try:
+            while 1:
+                os.kill(pid, SIGTERM)
+                time.sleep(0.1)
+        except OSError, err:
+            err = str(err)
+            if err.find("No such process") > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                print str(err)
+                sys.exit(1)
+
+    def restart(self):
+        """
+        Restart the daemon
+        """
+        self.stop()
+        self.start()
+
+    def run(self):
+        """
+        This is overridden in the subclass Daemon.
+        """
+
+
+class LensDaemon(Daemon):
+    def init(self):
+        print "init in LensDaemon calling super class init"
+        # Get lens config from environment variable or fail
+        self.lens = lens.Lens()
+
+    def run(self):
+        while True:
+            time.sleep(5)
+            self.lens.run()
+
+    def stop(self):
+        self.lens.stop()
+        Daemon.stop(self)
+
+
+if __name__ == "__main__":
+    daemon = LensDaemon('/tmp/daemon-example.pid')
+    daemon.init()
+
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            print "starting..."
+            #daemon.init()
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            print "stopping..."
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            #daemon.init()
+            daemon.restart()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+        sys.exit(0)
+
+    else:
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
--- lens/lens.py
+++ lens/lens.py
@@ -6,14 +6,23 @@ import json
 import ConfigParser
 import LensMongo as mdb
 import LensSqlite as sdb
-import LensAnalyzers
+import imp
 
+
+
+def getFilesPattern(path, extension="", contains=""):
+    allfiles = []
+    for path, subdirs, files in os.walk(path):
+        for filename in files:
+            f = os.path.join(path, filename)
+            if filename.endswith(extension) and filename.find(contains) >= 0:
+                allfiles.append((f, path, filename))
+    return allfiles
 
 class Lens:
     def __init__(self):
         self.dirs = {}
         self.files = {}
-        import pdb; pdb.set_trace()
         # Figure out where LENS_HOME is going to be.
         if 'LENS_HOME' not in os.environ:
             print 'Environment variable LENS_HOME not set, assuming current \
@@ -98,31 +107,57 @@ class Lens:
             self.logger.critical('Error initializing database: %s' % (str(e), ))
         self.loadAnalyzers()
 
-    def getFilesPattern(self, path, extension="", contains=""):
-        allfiles = []
-        for path, subdirs, files in os.walk(path):
-            for filename in files:
-                f = os.path.join(path, filename)
-                if filename.endswith(extension) and filename.find(contains) >= 0:
-                    allfiles.append((f, path, filename))
-        return allfiles
-
     def loadAnalyzers(self):
+        # https://docs.python.org/2/library/functions.html#getattr
         self.analyzers = {}
-        curanalyzers = self.getFilesPattern(self.dirs['analyzers'], '.py')
+        curanalyzers = getFilesPattern(self.dirs['analyzers'], '.py')
         for path_to_analyzer, directory, filename in curanalyzers:
             analyzername = 'analyzers.' + filename.replace('.py','')
-            analyzer = LensAnalyzers.load_source(analyzername, path_to_analyzer)
-            self.analyzers[analyzer.runWhenMatch()] = analyzer
+            module = imp.load_source(analyzername, path_to_analyzer)
+            for item in dir(module):
+                # ignore the internal items.
+                if not (item.startswith('__') and item.endswith('__')):
+                    details = getattr(module, item)()
+                    # If it is an analyzer, it must have these things
+                    requirements = set(['analyze', 'yara_sigs'])
+                    if set(dir(details)).intersection(requirements) == requirements:
+                        # instantiate this class because it seems to meet our
+                        # requirements of an analyzer
+                        if details.yara_sigs is not None:
+                            if len(details.yara_sigs) > 0:
+                                for sig in details.yara_sigs:
+                                    self.analyzers[sig] = getattr(module, item)()
+                                    self.logger.info('Successfully registered analyzer: %s:%s' % (filename, item))
+
+
+            # analyzer = LensAnalyzers.load_source(analyzername, path_to_analyzer)
+            # self.analyzers[analyzer.runWhenMatch()] = analyzer
 
     def findWork(self):
-        print "checking for work"
+        self.logger.info('checking for work')
+        allFiles = getFilesPattern(self.dirs['files'])
+        self.logger.info('checking for work in %s found %d files' % (self.dirs['files'],len(allFiles)))
+
+        for myfile in allFiles:
+            self.logger.info('Found file %s' % (myfile[2], ))
+
+        allUrls = getFilesPattern(self.dirs['urls'], '.txt')
+        for myurl in allUrls:
+            self.logger.info('Found urlfile %s' % (myurl[2], ))
+
+        allPcaps = getFilesPattern(self.dirs['pcaps'], '.pcap')
+        for mypcap in allPcaps:
+            self.logger.info('Found pcap %s' % (mypcap[2], ))
+
         # check each of the three work queues and handle any new work.
 
     def run(self):
-        self.logger.info('Logging initiated')
-        self.findWork(self)
+        self.logger.info('running...')
+        self.findWork()
         # if there is work, what kind
+
+    def stop(self):
+        self.logger.info('Lens has been stopped')
 
     def analyze(self):
         """Each file type supplies its own unique analysis techniques"""
